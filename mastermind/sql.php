dc608fa2d4be66aec6e74f9fb344c59b7cf0a0a7
ulrich-schroeders <ulrich.schroeders@gmail.com>
2024-01-14 11:47:22 +0100

    sql io

--- mastermind/sql.php
+++ mastermind/sql.php
@@ -3,7 +3,7 @@
 	header('Content-Type: application/json; charset=utf-8');
 	
 	function connectToSQL() {
-		$servername = "localhost";
+		$servername = "ulrich-schroeders.de.github.io";
 		$username = "d03b468f";
 		$password = "i*poe6Am6nlE89PAbhBmBrKW2";
 		$database = "d03b468f";
